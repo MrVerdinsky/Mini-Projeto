e841a9eeea7d507efa83bca810c7e8e8c886d9b8
MrVerdinsky <goncalo.vila.verde@hotmail.com>
2019-11-27 15:17:58 -0500

    Ficheiro do Stor

---
+++ sortify.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MSG_WELCOME "WELCOME TO SORTIFY!"
+#define MSG_SORT "Sort the following numbers:"
+#define MSG_SORT2 "Please sort the numbers"
+#define MSG_WELL "Well done!"
+#define MSG_WIN "Congratulations, you win!"
+#define MSG_OVER "Game Over."
+#define MSG_WRONG "Wrong answer."
+#define MSG_MAX "You have reached the maximum number of moves."
+#define MSG_BYE "Bye."
+#define MSG_UNKNOWN "Unknown option."
+/* Use puts() to print constant strings */
+
+int rand_number(const int, const int);
+void print_status(const int, const int, const int);
+void print_menu(void);
+
+
+int main()
+{
+/
+	puts(MSG_WELCOME);
+
+	return 0;
+}
+
+/* generate a random integer between min and max */
+int rand_number(const int min, const int max)
+{
+	if (max < min)
+	{
+		puts("Max must be larger than Min");
+		exit(0);
+	}
+	int n = abs(max - min) + 1;
+	return (rand() % n) + min;
+}
+
+/* print the game status */
+void print_status(const int level, const int score, const int plays)
+{
+	puts("+-----------------------------+");
+	printf("| level:  %02d                  |\n", level);
+	printf("| points: %02d                  |\n", score);
+	printf("| plays:  %02d                  |\n", plays);
+	puts("+-----------------------------+");
+}
+
+/* print the option menu */
+void print_menu(void)
+{
+	puts("+-----------------------------+");
+	puts("| SORTIFY                     |");
+	puts("| p - next chalenge           |");
+	puts("| q - quit                    |");
+	puts("| m - print this information  |");
+	puts("| s - show your status        |");
+	puts("+-----------------------------+");
+}
